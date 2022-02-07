f43941c1f5abaed02a16f67851847d54e85bb812
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-02-07 14:27:57 +0300

    Introducing MVC Architecture

---
+++ IntroNetCore/IntroNetCore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using IntroNetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.AdSoyad = "Türkay Ürkmez";
+            ViewBag.Saat = DateTime.Now.Hour;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Katil()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Katil(Katilimci katilimci)
+        {
+            if (ModelState.IsValid)
+            {
+                return Json("Kayit basarili");
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ IntroNetCore/IntroNetCore/Models/Katilimci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroNetCore.Models
+{
+    public class Katilimci
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Ad alanı boş olamaz")]
+        public string Ad { get; set; }
+        [Required(ErrorMessage = "Soyad alanı boş olamaz")]
+
+        public string Soyad { get; set; }
+        [EmailAddress]
+        public string Eposta { get; set; }
+        public bool? KatilimDurumu { get; set; }
+    }
+}
